0ec3620afe2969d982527ae2c96725a889a0408a
DenysSoldatenko <soldatenko911@gmail.com>
2023-07-10 08:30:00 +0300

    Added simple logic

---
+++ PasteBox/src/main/java/com/example/pastebox/controller/PasteBoxController.java
@@ -0,0 +1,27 @@
+package com.example.pastebox.controller;
+
+import com.example.pastebox.api.request.PasteBoxRequest;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RestController
+public class PasteBoxController {
+
+  @GetMapping("/")
+  public Collection<String> getPublicPasteList() {
+    return Collections.emptyList();
+  }
+
+  @GetMapping("/{hash}")
+  public String getByHash(@PathVariable String hash) {
+    return hash;
+  }
+
+  @PostMapping("/")
+  public String add(@RequestBody PasteBoxRequest request) {
+    return request.getData();
+  }
+}
