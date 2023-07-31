4284a5f22acede77948b3605901c8706e9fd3c97
DenysSoldatenko <soldatenko911@gmail.com>
2023-07-31 14:30:00 +0300

    Added EmailVerification project

---
+++ EmailVerificationSecuritySystem/src/main/java/com/example/securitysystem/EmailVerificationSecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailVerificationSecuritySystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmailVerificationSecuritySystemApplication.class, args);
+  }
+
+}
---
+++ EmailVerificationSecuritySystem/src/test/java/com/example/securitysystem/EmailVerificationSecuritySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailVerificationSecuritySystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
