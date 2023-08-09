4331fc13776f12a79df059318ed771ae1da2ae27
DenysSoldatenko <soldatenko911@gmail.com>
2023-08-09 15:45:00 +0300

    Implemented basic authentication

--- SpringSecuritySystem/src/main/java/com/example/springsecuritysystem/controllers/GreetingController.java
+++ SpringSecuritySystem/src/main/java/com/example/springsecuritysystem/controllers/GreetingController.java
@@ -1,4 +1,4 @@
-package com.example.springsecuritysystem;
+package com.example.springsecuritysystem.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
