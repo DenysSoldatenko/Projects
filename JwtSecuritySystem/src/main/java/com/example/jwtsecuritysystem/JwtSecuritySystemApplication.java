d22a6091b47822a65aac4ef3369f44d87b2802fd
DenysSoldatenko <soldatenko911@gmail.com>
2023-08-16 15:00:00 +0300

    Added JwtSecuritySystem project

---
+++ JwtSecuritySystem/src/main/java/com/example/jwtsecuritysystem/JwtSecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtsecuritysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecuritySystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JwtSecuritySystemApplication.class, args);
+  }
+
+}
