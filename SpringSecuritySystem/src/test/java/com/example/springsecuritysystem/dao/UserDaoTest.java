b9b7b8e2f82a5613e74b0cc65a993d7780e12c4c
DenysSoldatenko <soldatenko911@gmail.com>
2023-09-01 17:00:00 +0300

    Added tests to the dao in the SpringSecuritySystem project

---
+++ SpringSecuritySystem/src/test/java/com/example/springsecuritysystem/dao/UserDaoTest.java
@@ -0,0 +1,38 @@
+package com.example.springsecuritysystem.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Unit tests for the UserDao class.
+ */
+public class UserDaoTest {
+
+  private UserDao userDao;
+
+  @BeforeEach
+  public void setUp() {
+    userDao = new UserDao();
+  }
+
+  @Test
+  public void shouldFindUserByEmail() {
+    String email = "john.doe@gmail.com";
+    UserDetails userDetails = userDao.findUserByEmail(email);
+
+    assertEquals(email, userDetails.getUsername());
+    assertEquals(1, userDetails.getAuthorities().size());
+    assertEquals("ROLE_ADMIN", userDetails.getAuthorities().iterator().next().getAuthority());
+  }
+
+  @Test
+  public void shouldThrowUsernameNotFoundExceptionForNonexistentUser() {
+    String email = "nonexistent@gmail.com";
+    assertThrows(UsernameNotFoundException.class, () -> userDao.findUserByEmail(email));
+  }
+}
