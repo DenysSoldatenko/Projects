c19db1e73371dac6b9c19fb204bf340069c906e0
DenysSoldatenko <soldatenko911@gmail.com>
2023-09-14 09:30:00 +0300

    Added tests to the controllers in the JwtSecuritySystem project

---
+++ JwtSecuritySystem/src/test/java/com/example/jwtsecuritysystem/controllers/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.example.jwtsecuritysystem.controllers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.when;
+
+import com.example.jwtsecuritysystem.dto.UserDto;
+import com.example.jwtsecuritysystem.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Unit tests for the UserController class.
+ */
+@ExtendWith(SpringExtension.class)
+public class UserControllerTest {
+
+  @Mock
+  private UserService userService;
+  @Mock
+  private ModelMapper modelMapper;
+  private UserController userController;
+
+  @BeforeEach
+  public void setUp() {
+    userController = new UserController(userService, modelMapper);
+  }
+
+  @Test
+  public void testGetUserByIdNotFound() {
+    Long userId = 1L;
+    when(userService.findById(userId)).thenReturn(null);
+
+    ResponseEntity<UserDto> response = userController.getUserById(userId);
+
+    assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+    assertNull(response.getBody());
+  }
+}
