36cf6c79d607fabf637a50a93a6dcb7916537d4f
DenysSoldatenko <soldatenko911@gmail.com>
2023-09-17 17:45:00 +0300

    Added tests to the token in the EmailVerificationSecuritySystem project

---
+++ EmailVerificationSecuritySystem/src/test/java/com/example/securitysystem/registration/token/ConfirmationTokenServiceTest.java
@@ -0,0 +1,90 @@
+package com.example.securitysystem.registration.token;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.example.securitysystem.appuser.AppUser;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Unit tests for the ConfirmationTokenService class.
+ */
+@ExtendWith(MockitoExtension.class)
+public class ConfirmationTokenServiceTest {
+
+  @Mock
+  private ConfirmationTokenRepository confirmationTokenRepository;
+  private ConfirmationTokenService confirmationTokenService;
+
+  @BeforeEach
+  public void setUp() {
+    confirmationTokenService = new ConfirmationTokenService(confirmationTokenRepository);
+  }
+
+  @Test
+  public void testSaveConfirmationToken() {
+    ConfirmationToken token = new ConfirmationToken(
+        "token123",
+        LocalDateTime.now(),
+        LocalDateTime.now().plusHours(1),
+        new AppUser()
+    );
+    confirmationTokenService.saveConfirmationToken(token);
+
+    Mockito.verify(confirmationTokenRepository).save(token);
+  }
+
+  @Test
+  public void testGetToken() {
+    String tokenValue = "token123";
+    ConfirmationToken token = new ConfirmationToken(
+        tokenValue,
+        LocalDateTime.now(),
+        LocalDateTime.now().plusHours(1),
+        new AppUser()
+    );
+    Mockito.when(confirmationTokenRepository.findByToken(tokenValue))
+        .thenReturn(Optional.of(token));
+
+    Optional<ConfirmationToken> retrievedToken = confirmationTokenService.getToken(tokenValue);
+
+    assertTrue(retrievedToken.isPresent());
+    assertEquals(tokenValue, retrievedToken.get().getToken());
+  }
+
+  @Test
+  public void testFindNonExpiredToken() {
+    AppUser appUser = new AppUser();
+    LocalDateTime createdAt = LocalDateTime.now().minusMinutes(30);
+    LocalDateTime expiresAt = LocalDateTime.now().plusHours(1);
+    ConfirmationToken token = new ConfirmationToken("token123", createdAt, expiresAt, appUser);
+
+    Mockito.when(confirmationTokenRepository.findFirstByAppUserAndConfirmedAtIsNotNull(appUser))
+        .thenReturn(Optional.of(token));
+
+    Optional<ConfirmationToken> nonExpiredToken
+        = confirmationTokenService.findNonExpiredToken(appUser);
+
+    assertTrue(nonExpiredToken.isPresent());
+    assertEquals("token123", nonExpiredToken.get().getToken());
+  }
+
+  @Test
+  public void testSetConfirmedAt() {
+    String tokenValue = "token123";
+    ConfirmationToken token = new ConfirmationToken(tokenValue, LocalDateTime.now(),
+        LocalDateTime.now().plusHours(1), new AppUser());
+
+    confirmationTokenService.setConfirmedAt(tokenValue);
+
+    assertNull(token.getConfirmedAt());
+  }
+}
---
+++ EmailVerificationSecuritySystem/src/test/java/com/example/securitysystem/registration/token/ConfirmationTokenTest.java
@@ -0,0 +1,68 @@
+package com.example.securitysystem.registration.token;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import com.example.securitysystem.appuser.AppUser;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the ConfirmationToken class.
+ */
+public class ConfirmationTokenTest {
+
+  @Test
+  public void testConstructorAndGetterMethods() {
+    AppUser appUser = new AppUser();
+    LocalDateTime createdAt = LocalDateTime.now();
+    LocalDateTime expiresAt = LocalDateTime.now().plusHours(1);
+
+    ConfirmationToken confirmationToken
+        = new ConfirmationToken("token123", createdAt, expiresAt, appUser);
+
+    assertEquals("token123", confirmationToken.getToken());
+    assertEquals(createdAt, confirmationToken.getCreatedAt());
+    assertEquals(expiresAt, confirmationToken.getExpiresAt());
+    assertNull(confirmationToken.getConfirmedAt());
+    assertEquals(appUser, confirmationToken.getAppUser());
+  }
+
+  @Test
+  public void testNoArgsConstructor() {
+    ConfirmationToken confirmationToken = new ConfirmationToken();
+
+    assertNull(confirmationToken.getToken());
+    assertNull(confirmationToken.getCreatedAt());
+    assertNull(confirmationToken.getExpiresAt());
+    assertNull(confirmationToken.getConfirmedAt());
+    assertNull(confirmationToken.getAppUser());
+  }
+
+  @Test
+  public void testId() {
+    AppUser appUser = new AppUser();
+    LocalDateTime createdAt = LocalDateTime.now();
+    LocalDateTime expiresAt = LocalDateTime.now().plusHours(1);
+
+    ConfirmationToken confirmationToken
+        = new ConfirmationToken("token123", createdAt, expiresAt, appUser);
+    confirmationToken.setId(1L);
+
+    assertEquals(1L, confirmationToken.getId());
+  }
+
+  @Test
+  public void testConfirmedAt() {
+    AppUser appUser = new AppUser();
+    LocalDateTime createdAt = LocalDateTime.now();
+    LocalDateTime expiresAt = LocalDateTime.now().plusHours(1);
+    LocalDateTime confirmedAt = LocalDateTime.now();
+
+    ConfirmationToken confirmationToken
+        = new ConfirmationToken("token123", createdAt, expiresAt, appUser);
+    confirmationToken.setConfirmedAt(confirmedAt);
+
+    assertEquals(confirmedAt, confirmationToken.getConfirmedAt());
+  }
+}
