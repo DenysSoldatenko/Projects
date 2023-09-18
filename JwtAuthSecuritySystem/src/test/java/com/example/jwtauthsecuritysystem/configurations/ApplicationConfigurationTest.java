00419da29665d1ab91756f026b1956c706495bad
DenysSoldatenko <soldatenko911@gmail.com>
2023-09-18 16:30:00 +0300

    Added tests to the configurations in the JwtAuthSecuritySystem project

---
+++ JwtAuthSecuritySystem/src/test/java/com/example/jwtauthsecuritysystem/configurations/ApplicationConfigurationTest.java
@@ -0,0 +1,44 @@
+package com.example.jwtauthsecuritysystem.configurations;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.example.jwtauthsecuritysystem.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+class ApplicationConfigurationTest {
+
+  @Test
+  void testUserDetailsServiceBean() {
+    UserRepository userRepository = Mockito.mock(UserRepository.class);
+    ApplicationConfiguration applicationConfiguration
+        = new ApplicationConfiguration(userRepository);
+    UserDetailsService userDetailsService = applicationConfiguration.userDetailsService();
+
+    assertNotNull(userDetailsService);
+  }
+
+  @Test
+  void testAuthenticationProviderBean() {
+    UserRepository userRepository = Mockito.mock(UserRepository.class);
+    ApplicationConfiguration applicationConfiguration
+        = new ApplicationConfiguration(userRepository);
+    AuthenticationProvider authenticationProvider
+        = applicationConfiguration.authenticationProvider();
+
+    assertNotNull(authenticationProvider);
+  }
+
+  @Test
+  void testPasswordEncoderBean() {
+    UserRepository userRepository = Mockito.mock(UserRepository.class);
+    ApplicationConfiguration applicationConfiguration
+        = new ApplicationConfiguration(userRepository);
+    PasswordEncoder passwordEncoder = applicationConfiguration.passwordEncoder();
+
+    assertNotNull(passwordEncoder);
+  }
+}
---
+++ JwtAuthSecuritySystem/src/test/java/com/example/jwtauthsecuritysystem/configurations/jwt/JwtServiceIntegrationTest.java
@@ -0,0 +1,50 @@
+package com.example.jwtauthsecuritysystem.configurations.jwt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class JwtServiceIntegrationTest {
+
+  @Autowired
+  private JwtService jwtService;
+
+  @Test
+  void testGenerateAndValidateToken() {
+    UserDetails userDetails = User.withUsername("testuser")
+        .password("password")
+        .authorities("ROLE_USER")
+        .build();
+
+    String token = jwtService.generateToken(userDetails);
+
+    assertTrue(jwtService.isTokenValid(token, userDetails));
+  }
+
+  @Test
+  void testGenerateAndExtractClaim() {
+    UserDetails userDetails = User.withUsername("testuser")
+        .password("password")
+        .authorities("ROLE_USER")
+        .build();
+
+    Map<String, Object> extraClaims = new HashMap<>();
+    extraClaims.put("customClaim", "someValue");
+    String token = jwtService.generateToken(extraClaims, userDetails);
+
+    String customClaimValue = jwtService
+        .extractClaim(token, claims -> claims.get("customClaim", String.class));
+
+    assertEquals("someValue", customClaimValue);
+  }
+}
