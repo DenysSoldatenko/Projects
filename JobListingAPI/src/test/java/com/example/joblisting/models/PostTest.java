d8ce8eddc51b17273de312c8575015c3d72914d8
DenysSoldatenko <soldatenko911@gmail.com>
2023-09-20 16:30:00 +0300

    Added tests to the models in the JobListingApi project

---
+++ JobListingAPI/src/test/java/com/example/joblisting/models/PostTest.java
@@ -0,0 +1,29 @@
+package com.example.joblisting.models;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the Post class.
+ */
+public class PostTest {
+
+  @Test
+  public void testGettersAndSetters() {
+    Post post = new Post();
+
+    post.setId("123");
+    post.setProfile("Software Engineer");
+    post.setDescription("Job description here");
+    post.setExperience(3);
+    post.setTechnologies(new String[]{"Java", "Spring Boot"});
+
+    assertEquals("123", post.getId());
+    assertEquals("Software Engineer", post.getProfile());
+    assertEquals("Job description here", post.getDescription());
+    assertEquals(3, post.getExperience());
+    assertEquals("Java", post.getTechnologies()[0]);
+    assertEquals("Spring Boot", post.getTechnologies()[1]);
+  }
+}
