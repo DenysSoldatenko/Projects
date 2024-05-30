e99c952c71ea4841b2db0fb5ff72afa7f5358166
DenysSoldatenko <soldatenko911@gmail.com>
2024-05-30 09:45:00 +0300

    Updated the test in the AccountApi

--- AccountAPI/src/test/java/com/example/accountapi/initializers/DatabaseInitializerTest.java
+++ AccountAPI/src/test/java/com/example/accountapi/initializers/DatabaseInitializerTest.java
@@ -3,8 +3,11 @@ package com.example.accountapi.initializers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.example.accountapi.models.Account;
 import com.example.accountapi.repositories.AccountRepository;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -25,15 +28,15 @@ class DatabaseInitializerTest {
     accountRepository.deleteAll();
   }
 
-//  @Test
-//  void shouldInsertTenAccountsIntoDatabase() throws Exception {
-//    loadDatabase.initDatabase().run((String) null);
-//    List<Account> accounts = accountRepository.findAll();
-//
-//    assertEquals(10, accounts.size());
-//
-//    for (Account account : accounts) {
-//      assertNotNull(account.getAccountNumber());
-//    }
-//  }
+  @Test
+  void shouldInsertTenAccountsIntoDatabase() {
+    databaseInitializer.initDatabase();
+    List<Account> accounts = accountRepository.findAll();
+
+    assertEquals(10, accounts.size());
+
+    for (Account account : accounts) {
+      assertNotNull(account.getAccountNumber());
+    }
+  }
 }
--- AccountAPI/src/test/java/com/example/accountapi/services/AccountServiceImplTest.java
+++ AccountAPI/src/test/java/com/example/accountapi/services/AccountServiceImplTest.java
@@ -7,12 +7,12 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.example.accountapi.initializers.DatabaseInitializer;
 import com.example.accountapi.models.Account;
 import com.example.accountapi.repositories.AccountRepository;
+import com.example.accountapi.services.impl.AccountServiceImpl;
 import java.util.List;
 import java.util.Optional;
-
-import com.example.accountapi.services.impl.AccountServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,6 +29,9 @@ class AccountServiceImplTest {
 
   @Mock
   private AccountRepository accountRepository;
+
+  @Mock
+  private DatabaseInitializer databaseInitializer;
 
   @InjectMocks
   private AccountServiceImpl accountServiceImpl;
@@ -57,7 +60,6 @@ class AccountServiceImplTest {
     account.setId(1);
 
     when(accountRepository.findById(1)).thenReturn(Optional.of(account));
-
     Account foundAccount = accountServiceImpl.getAccountById(1);
 
     assertEquals(account, foundAccount);
@@ -66,7 +68,6 @@ class AccountServiceImplTest {
   @Test
   void shouldThrowExceptionWhenAccountNotFound() {
     when(accountRepository.findById(1)).thenReturn(Optional.empty());
-
     assertThrows(ResponseStatusException.class, () -> accountServiceImpl.getAccountById(1));
   }
 
@@ -83,51 +84,39 @@ class AccountServiceImplTest {
 
   @Test
   void shouldWithdrawFromAccount() {
-    // Prepare test data
     Account account = new Account();
     account.setId(1);
     account.setBalance(100.0f);
 
-    // Mock the findById behavior to return the account
     when(accountRepository.findById(1)).thenReturn(Optional.of(account));
-
-    // Define behavior for accountRepository.withdraw to deduct the amount
     doAnswer(invocation -> {
       float amountToWithdraw = invocation.getArgument(0);
       account.setBalance(account.getBalance() - amountToWithdraw);
       return null;
     }).when(accountRepository).withdraw(100.0f, 1);
-
-    // Call the withdrawal method
     Account updatedAccount = accountServiceImpl.withdrawFunds(100.0f, 1);
 
     assertEquals(account, updatedAccount);
     assertEquals(0.0f, updatedAccount.getBalance());
   }
 
-
   @Test
   void shouldDepositToAccount() {
-    // Prepare test data
     Account account = new Account();
     account.setId(1);
     account.setBalance(100.0f);
 
-    // Mock the findById behavior to return the account
     when(accountRepository.findById(1)).thenReturn(Optional.of(account));
-
-    // Define behavior for accountRepository.save to update the balance
     doAnswer(invocation -> {
       float amountToDeposit = invocation.getArgument(0);
-      account.setBalance(account.getBalance() + amountToDeposit);
+      if (account.getId().equals(invocation.getArgument(1))) {
+        account.setBalance(account.getBalance() + amountToDeposit);
+      }
       return null;
     }).when(accountRepository).deposit(100.0f, 1);
-
-    // Call the deposit method
     Account updatedAccount = accountServiceImpl.addFunds(100.0f, 1);
 
-    // Assert the result
-    assertEquals(account, updatedAccount);
+    verify(accountRepository, times(1)).deposit(100.0f, 1);
     assertEquals(200.0f, updatedAccount.getBalance());
   }
 
@@ -137,7 +126,15 @@ class AccountServiceImplTest {
     account.setId(1);
 
     accountServiceImpl.removeAccount(1);
-
     verify(accountRepository, times(1)).deleteById(1);
+  }
+
+  @Test
+  void shouldInitializeData() {
+    String expectedMessage = "Data initialized successfully";
+    when(databaseInitializer.initDatabase()).thenReturn(expectedMessage);
+
+    String result = accountServiceImpl.initializeData();
+    assertEquals(expectedMessage, result);
   }
 }
