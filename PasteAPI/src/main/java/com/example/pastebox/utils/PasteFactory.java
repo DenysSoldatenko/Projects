739a4c18790315741d95e79a0bc734d394584003
DenysSoldatenko <soldatenko911@gmail.com>
2024-07-24 16:15:00 +0300

    Added the missing Javadocs to the PasteAPI

--- PasteAPI/src/main/java/com/example/pastebox/utils/PasteFactory.java
+++ PasteAPI/src/main/java/com/example/pastebox/utils/PasteFactory.java
@@ -7,9 +7,21 @@ import com.example.pastebox.models.Paste;
 import com.example.pastebox.models.PasteDto;
 import lombok.experimental.UtilityClass;
 
+/**
+ * Utility class for creating {@link Paste} objects.
+ * Provides a method to generate a {@link Paste} instance from a {@link PasteDto} object.
+ */
 @UtilityClass
 public class PasteFactory {
 
+  /**
+   * Generates a {@link Paste} instance based on the provided {@link PasteDto}.
+   * The expiration time and short link are automatically generated.
+   *
+   * @param pasteDto The {@link PasteDto} object containing the details for creating a {@link Paste}.
+   * @return A new {@link Paste} instance with the provided content, creation time, expiration time,
+   *         expiration duration, public status, and a generated short link.
+   */
   public static Paste generatePaste(PasteDto pasteDto) {
     return Paste.builder()
       .content(pasteDto.getContent())
