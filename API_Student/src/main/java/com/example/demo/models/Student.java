215221aeff1e55b3dac8479d4b6931dfcee2b6d7
DenysSoldatenko <soldatenko911@gmail.com>
2024-09-14 10:00:00 +0300

    Renamed StudentAPI to API_Student

--- API_Student/src/main/java/com/example/demo/models/Student.java
+++ API_Student/src/main/java/com/example/demo/models/Student.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
