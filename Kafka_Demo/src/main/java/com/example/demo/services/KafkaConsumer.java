dd758b0201790a155f1922e0043581a35301cd92
DenysSoldatenko <soldatenko911@gmail.com>
2024-09-22 11:00:00 +0300

    Added Swagger to the Kafka_Demo project

--- Kafka_Demo/src/main/java/com/example/demo/services/KafkaConsumer.java
+++ Kafka_Demo/src/main/java/com/example/demo/services/KafkaConsumer.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import static java.lang.String.format;
-
 /**
  * Service class for consuming messages from Kafka.
  */
