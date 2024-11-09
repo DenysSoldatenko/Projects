0a71862d0f36be0b188922b99d0f639d2646eb17
DenysSoldatenko <soldatenko911@gmail.com>
2024-11-09 17:00:00 +0300

    Added the Telegram_MilitaryTrackerBot project

---
+++ Telegram_MilitaryTrackerBot/src/main/java/com/example/militarytrackerbot/TelegramMilitaryTrackerBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.militarytrackerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramMilitaryTrackerBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TelegramMilitaryTrackerBotApplication.class, args);
+  }
+
+}
